59eabe6eac2dfedd2a9ef734a7baa1e5ea031f84
cklxx <1293822641@qq.com>
2025-06-10 21:54:46 +0800

    清理测试文件

--- test_format.py
+++
@@ -1,7 +0,0 @@
-def test_function():
-    x = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
-    if x > 10:
-        print("x is greater than 10")
-    else:
-        print("x is not greater than 10")
-    return x
